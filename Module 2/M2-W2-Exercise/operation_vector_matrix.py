ec6f55076810fc0d376f86e4c34497841c68ce9b
ThuVDT <thuhoctap1713@gmail.com>
2024-07-21 12:29:49 +0700

    feature/Module02-Week02-Exercise - M2W2Ex

---
+++ Module 2/M2-W2-Exercise/operation_vector_matrix.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def compute_vector_length(vector):
+    len_of_vector = np.linalg.norm(vector)
+    return len_of_vector
+
+
+def compute_dot_product(vector1, vector2):
+    result = np.dot(vector1, vector2)
+    return result
+
+
+def matrix_multi_vector(matrix, vector):
+    result = np.dot(matrix, vector)
+    return result
+
+
+def matrix_multi_matrix(matrix1, matrix2):
+    result = np.dot(matrix1, matrix2)
+    return result
+
+
+def inverse_matrix(matrix):
+    result = np.linalg.inv(matrix)
+    return result
